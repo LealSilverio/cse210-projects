a07e3c0a80187a13ebc5a93a5dd6ea394f84f40e
Sara Silverio <saralealsilverio@gmail.com>
2023-04-19 13:40:46 -0600

    Prep 2 update - Core Requirements Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        string letter = " ";
+             if (grade >=70)
+            {
+                if (grade >= 90)
+                {
+                    letter = "an A!";
+                }
+                else if (grade >= 80)
+                {
+                    letter = "a B";
+                }
+                else
+                {
+                    letter = "a C";
+                }
+                Console.WriteLine("Congrats! You passed the class");
+            }
+            else 
+            {
+                if (grade >= 60)
+                {
+                    letter = "a D";
+                }
+                else
+                {
+                    letter = "a F";
+                }
+                Console.WriteLine("You failed the class");
+            }
+            Console.WriteLine($"You got {letter}");
     }
 }
