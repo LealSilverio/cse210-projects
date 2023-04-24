15b59714be3b4140c7ee1660d21600f30f1b0182
Sara Silverio <saralealsilverio@gmail.com>
2023-04-24 17:10:54 -0600

    Core Requirements - Prep 3, 4, and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("Welcome to Guess My Number");
+
+        Random randomNumber = new Random(); 
+        int number = randomNumber.Next(1, 100);
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userImput = Console.ReadLine();
+            guess = int.Parse(userImput);
+    
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != number);
+
+        if (guess == number)
+        {
+            Console.WriteLine("You got it! Nice!!");
+        }
+    }   
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        
+        List<int> numbers = new List<int>();
+        int number;
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            string userImput = Console.ReadLine();
+            number = int.Parse(userImput);
+            numbers.Add(number);
+        } while (number != 0);
+        
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
