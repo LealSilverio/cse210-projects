1c3f421162cab4f3fcae308deb19de01f31f427a
Sara Silverio <saralealsilverio@gmail.com>
2023-05-01 12:20:57 -0600

    prep week03

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Job
+{
+  public string company;
+  public string jobTitle;
+  public int startYear;
+  public int endYear;
+
+  public void Display()
+  {
+    Console.WriteLine($"{jobTitle} ({company}) {startYear}-{endYear}");
+  }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+    Job job1 = new Job();
+    job1.jobTitle = "Software Engineer";
+    job1.company = "Microsoft";
+    job1.startYear = 2019; 
+    job1.endYear = 2022;
+
+
+    Job job2 = new Job();
+    job2.jobTitle = "Manager";
+    job2.company = "Apple";
+    job2.startYear = 2022; 
+    job2.endYear = 2023;
+
+    Resume member1 = new Resume();
+    member1.name = "Allison Rose";
+    
+    member1.jobs.Add(job1);
+    member1.jobs.Add(job2);
+
+    member1.Display();
     }
 }
