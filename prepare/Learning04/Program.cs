840e57f7650b9c49e50e6a5caa96a25b91e30c53
Sara Silverio <saralealsilverio@gmail.com>
2023-05-31 11:18:48 -0600

    learning 04 finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+        
+        MathAssignment ma = new MathAssignment("Elizabeth Smith", "Fraction", "7.3", "8-9");
+        Console.WriteLine(ma.GetSummary());
+        Console.WriteLine(ma.GetHomeworkList());
+
+        WritingAssignment wa = new WritingAssignment("Mary Waters","European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
     }
 }
